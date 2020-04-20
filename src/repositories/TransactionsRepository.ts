54823f94505842d8750db6ddb6b4cf5974e85a73
monteiroluana <luanamonteiropereira95@gmail.com>
2020-04-19 22:41:32 -0300

    resolved

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,28 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((sum, transaction) => {
+      return transaction.type === 'income' ? sum + transaction.value : sum;
+    }, 0);
+
+    const outcome = this.transactions.reduce((sum, transaction) => {
+      return transaction.type === 'outcome' ? sum + transaction.value : sum;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
